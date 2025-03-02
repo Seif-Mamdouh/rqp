09ebdaa75fd9fa5a4200ad1e268c325ef63f44bc
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2025-03-01 22:45:43 -0500

    updateToDo by id

--- app/components/mutate.tsx
+++ app/components/mutate.tsx
@@ -2,6 +2,12 @@ import { useMutation } from '@tanstack/react-query';
 import { Todo } from './dataList';
 
 async function updateTodo(todo: Todo) {
+    const todosString = window.localStorage.getItem("todos") || "[]";
+    const totalTodos: Todo[] = JSON.parse(todosString);
+    
+    const toDoById = totalTodos.find((t: Todo) => t.id === todo.id);
+    
+    return toDoById;
 }
 
 export default function useUpdateTodo() {
