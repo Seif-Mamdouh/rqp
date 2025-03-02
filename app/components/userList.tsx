15ef7d8dbb29f463f05ee05dc98d1f71f4b28ca6
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2025-03-02 00:22:36 -0500

    filtering

--- app/components/userList.tsx
+++ app/components/userList.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import useFetchUsers from './fetchUserData';
 
 export interface User {
@@ -11,13 +12,22 @@ export interface User {
 export default function UserList() {
     const { data: users, isLoading, error } = useFetchUsers();
 
+    const [search, setSearch] = useState('');
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    }
+
+    const filteredUsers = users?.filter((user: User) => user.name.toLowerCase().includes(search.toLowerCase()));
+
     if (isLoading) return <div>Loading...</div>;
 
     if (error) return <div>Error: {error.message}</div>;
 
     return (
         <ul className="space-y-4">
-            {users?.map((user: User) => (
+            <input type="text" placeholder="Search" onChange={handleSearch} />
+            {filteredUsers?.map((user: User) => (
                 <li key={user.id} className="p-4 border rounded">
                     <h3 className="font-bold">{user.name}</h3>
                     <p className="text-gray-600">{user.email}</p>
